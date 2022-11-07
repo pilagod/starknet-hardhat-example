6ee2028a4bcbdf07fbf637b239f9bc404193f8e7
pilagod <pilagooood@gmail.com>
2022-11-07 17:01:10 +0800

    add devnet ETH address

--- test/constants.ts
+++ test/constants.ts
@@ -1,2 +1,4 @@
 /** 15 min */
 export const TIMEOUT = 900_000;
+// https://shard-labs.github.io/starknet-devnet/docs/guide/mint-token
+export const ETH_ADDRESS = "0x62230ea046a9a5fbc261ac77d03c8d41e5d442db2284587570ab46455fd2488";
